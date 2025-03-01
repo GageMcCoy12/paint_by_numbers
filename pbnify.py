35b33855da9bb0d4375864e7127f746798b098e4
gage mccoy <75149938+GageMcCoy12@users.noreply.github.com>
2025-03-01 00:10:54 -0700

    Update pbnify.py

--- pbnify.py
+++ pbnify.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 
-import .dominant_cluster
-import .image_utils
-import .process
+from . import dominant_cluster
+from . import image_utils
+from . import process
 
 
 def simple_matrix_to_image(mat, palette):
